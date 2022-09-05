28217c6526dc300c4f1852a701de10c252a102d5
Cooper <cooper.pecot@gmail.com>
2022-09-05 16:46:10 -0600

    Added Error Handling

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const path = require('path')
 const mongoose = require('mongoose')
 const engine = require('ejs-mate')
 const methodOverride = require('method-override')
+
+const catchAsync = require('./utilities/catchAsync')
+const ExpressError = require('./utilities/ExpressError')
 const Asset = require('./models/asset')
 
 async function main() {
@@ -13,8 +16,6 @@ async function main() {
 }
 
 main().catch((err) => console.log(err))
-
-app.listen(3000)
 
 app.engine('ejs', engine)
 app.set('view engine', 'ejs')
@@ -30,13 +31,13 @@ app.get('/', (req, res) => {
 })
 
 // Index
-app.get('/assets', async (req, res) => {
+app.get('/assets', catchAsync(async (req, res) => {
   const phones = await Asset.find({ category: 'Phone' })
   const vehicles = await Asset.find({ category: 'Vehicle' })
   const tools = await Asset.find({ category: 'Tool' })
   const lapTab = await Asset.find({ category: 'Laptop/Tablet' })
   res.render('assets/index', { phones, vehicles, tools, lapTab })
-})
+}))
 
 // New
 app.get('/assets/new', (req, res) => {
@@ -44,40 +45,49 @@ app.get('/assets/new', (req, res) => {
 })
 
 // Create
-app.post('/assets', async (req, res) => {
+app.post('/assets', catchAsync(async (req, res, next) => {
+  if (!req.body.asset) throw new ExpressError(400, 'Invalid Asset Data')
   const a = new Asset(req.body.asset)
-  a.save()
+  await a.save()
   res.redirect('/assets')
-})
+}))
 
 // Show
-app.get('/assets/:id', async (req, res) => {
+app.get('/assets/:id', catchAsync(async (req, res) => {
   const { id } = req.params
   const a = await Asset.findById(id)
   res.render('assets/show', { a })
-})
+}))
 
 // Edit
-app.get('/assets/:id/edit', async (req, res) => {
+app.get('/assets/:id/edit', catchAsync(async (req, res) => {
   const { id } = req.params
   const a = await Asset.findById(id)
   res.render('assets/edit', { a, categories })
-})
+}))
 
 // Update
-app.patch('/assets/:id', async (req, res) => {
+app.patch('/assets/:id', catchAsync(async (req, res) => {
   const { id } = req.params
   await Asset.findByIdAndUpdate(id, req.body.asset)
   res.redirect(`/assets/${id}`)
-})
+}))
 
 // Destroy
-app.delete('/assets/:id', async (req, res) => {
+app.delete('/assets/:id', catchAsync(async (req, res) => {
   const { id } = req.params
   await Asset.findByIdAndRemove(id)
   res.redirect('/assets')
+}))
+
+app.all('*', (req, res, next) => {
+  next(new ExpressError(404, 'Page Not Found'))
 })
 
-app.use((req, res) => {
-  res.status(404).render('err/404')
+app.use((err, req, res, next) => {
+  const { status = 500 } = err
+  if (!err.message) err.message = 'Something went Wrong'
+  res.status(status).render('err/error', { err })
 })
+
+app.listen(3000)
